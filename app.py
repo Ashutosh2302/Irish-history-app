d777b65fcf6416db19ef1e6577e655fa42570ba3
Ashutosh <ashutoshb2000@gmail.com>
2021-11-16 10:28:39 +0000

    query executor

--- app.py
+++ app.py
@@ -6,21 +6,16 @@ from dash.dependencies import Input, Output, State
 import requests
 from SPARQLWrapper import SPARQLWrapper, JSON
 import string
+from Query_executor import QueryExecutor
 
-selectAllCountiesQuery = '''PREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0A
-PREFIX%20ours%3A%20%3Chttp%3A%2F%2Fwww.semanticweb.org%2Fashutoshbansal%2Fontologies%2F2021%2F10%2Fproject-ontology%3E%0A
-select DISTINCT ?county where {
-	?county rdf:type ours:county .
-}'''
 
-urlForSelectAllCounties = f'http://LAPTOP-45QEIGK2:7200/repositories/KDE-project?query={selectAllCountiesQuery}'  # Needs to be updated for every separate local machine
-#
-# @lru_cache(maxsize=10)
-def query():
-    res = requests.request('GET', urlForSelectAllCounties)
-    print(res.text)
-    counties = [res.text]  # Need to convert this string into an array of individual values
-    print(counties)
+
+# # @lru_cache(maxsize=10)
+# def query():
+#     # res = requests.request('GET', urlForSelectAllCounties)
+#     # print(res.text)
+#     # counties = [res.text]  # Need to convert this string into an array of individual values
+#     print(counties)
 
 
 query_1 = {'poi': '', 'location-type': '', 'location': ''}
@@ -36,56 +31,10 @@ app.title = 'Irish History'
 points_of_interest = ['Pilgrim Path', 'Museum', 'Walled Towns', 'Landmarks']
 location = ['Town', 'County']
 
+data = QueryExecutor().loading_default_data()
+counties = data[0]
+towns = data[1]
 
-counties = [
-                 {'label': 'Antrim', 'value': 'Antrim'},
-                 {'label': 'Armagh', 'value': 'ARM'},
-                 {'label': 'Carlow', 'value': 'CAR'},
-                 {'label': 'Cavan', 'value': 'CAV'},
-                 {'label': 'Clare', 'value': 'CLA'},
-                 {'label': 'Cork', 'value': 'COR'},
-                 {'label': 'Derry', 'value': 'DER'},
-                 {'label': 'Donegal', 'value': 'DON'},
-                 {'label': 'Down', 'value': 'DOW'},
-                 {'label': 'Dublin', 'value': 'DUB'},
-                 {'label': 'Fermanagh', 'value': 'FER'},
-                 {'label': 'Galway', 'value': 'GAL'},
-                 {'label': 'Kerry', 'value': 'KER'},
-                 {'label': 'Kildare', 'value': 'KIL'},
-                 {'label': 'Kilkenny', 'value': 'KILK'},
-                 {'label': 'Laois', 'value': 'LAO'},
-                 {'label': 'Leitrim', 'value': 'LEI'},
-                 {'label': 'Limerick', 'value': 'LIM'},
-                 {'label': 'Longford', 'value': 'LON'},
-                 {'label': 'Louth', 'value': 'LOU'},
-                 {'label': 'Mayo', 'value': 'MAY'},
-                 {'label': 'Meath', 'value': 'MEA'},
-                 {'label': 'Monaghan', 'value': 'MON'},
-                 {'label': 'Offaly', 'value': 'OFF'},
-                 {'label': 'Roscommon', 'value': 'ROS'},
-                 {'label': 'Sligo', 'value': 'SLI'},
-                 {'label': 'Tipperary', 'value': 'TIP'},
-                 {'label': 'Tyrone', 'value': 'TYR'},
-                 {'label': 'Waterford', 'value': 'WAT'},
-                 {'label': 'Westmeath', 'value': 'WES'},
-                 {'label': 'Wexford', 'value': 'WEX'},
-                 {'label': 'Wicklow', 'value': 'WIC'}
-             ]
-towns = [
-    {'label': 'Ardmore', 'value': 'ARD'},
-    {'label': 'Ballintubber', 'value': 'BAL'},
-    {'label': 'Ballycumber', 'value': 'BALC'},
-    {'label': 'Ballymacavany', 'value': 'BALM'},
-    {'label': 'Bunglass', 'value': 'BUN'},
-    {'label': 'Caherciveen', 'value': 'CAHE'},
-    {'label': 'Corofin', 'value': 'COR'},
-    {'label': 'Drimoleague', 'value': 'DRI'},
-    {'label': 'Glencolumbkille', 'value': 'Glen'},
-    {'label': 'Hollywood', 'value': 'HOL'},
-    {'label': 'Kilcommon', 'value': 'KILK'},
-    {'label': 'Kildare Town', 'value': 'KIL'},
-    {'label': 'Ventry', 'value': 'VEN'}
-]
 app.layout = html.Div([
 
     html.Div(
@@ -316,7 +265,6 @@ app.layout = html.Div([
     ),
 
 
-
     html.H2(children="List names of Poi associated with year/historicYear that any other poi is associated with",
             style={'textAlign': 'center', 'verticalAlign': 'middle', 'line-height': '20vh'}),
     html.Div(
@@ -388,33 +336,7 @@ def update_output(n_clicks, poi, location_type, location):
     query_1['location'] = location
     print(f"query 1: {query_1}")
 
-    sparql = SPARQLWrapper("http://LAPTOP-45QEIGK2:7200/repositories/KDE-project")
-    sparql.setQuery("""
-       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX ours: <http://www.semanticweb.org/ashutoshbansal/ontologies/2021/10/project-ontology>
-SELECT ?name WHERE {
-    ?s rdf:type ?o .
-    ?s ours:name ?name .
-    ?s ours:locatedIn <http://www.semanticweb.org/ashutoshbansal/ontologies/2021/10/project-ontology#countyKerry> .
-    FILTER(?o IN (ours:pilgrimPath, ours:walledTown, ours:landmark))
-}
-ORDER BY ASC(UCASE(str(?name)))
-
-
-    """)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-
-    print(results)
-
-
-    # query = string.Template(query).substitute(COUNTY=location)
-
-    # print(f'http://LAPTOP-45QEIGK2:7200/repositories/KDE-project?query={query}')
-    # res = requests.request('GET', f'http://LAPTOP-45QEIGK2:7200/repositories/KDE-project?query={query}')
-    # print(res.text)
-    # counties = [res.text]  # Need to convert this string into an array of individual values
-    # print(counties)
+    QueryExecutor().getPointsOfInterestInPlace(poi, location)
 
     return f'POI: {poi}, location type: {location_type}, location: {location}'
 
